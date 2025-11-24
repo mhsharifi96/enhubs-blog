6e6ec817ff3cb72cf63fd73bbe21a854a695aff8
mhsharifi96 <mh.sh7676@gmail.com>
2025-11-24 23:14:34 +0330

    change base_url

--- src/lib/blog.ts
+++ src/lib/blog.ts
@@ -1,4 +1,4 @@
-const DEFAULT_BASE_URL = "http://127.0.0.1:8000/api/blog";
+const DEFAULT_BASE_URL = "https://api.langagent.ir/api/blog";
 const rawBaseUrl =
   process.env.API_BASE_URL ?? process.env.NEXT_PUBLIC_API_BASE_URL ?? DEFAULT_BASE_URL;
 const normalizedBaseUrl = rawBaseUrl.replace(/\/$/, "");
